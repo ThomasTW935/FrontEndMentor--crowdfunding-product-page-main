f05b2392837f7a385cfaff471762b2017bec9627
ThomasTW935 <thomastw935@gmail.com>
2021-03-01 00:33:31 +0800

    Update Pledge and completed modal design completed

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-console.log(123)
 toggleModal()
 
 function toggleModal(){
-    let buttons = document.querySelectorAll('.header__toggle')
+    let buttons = document.querySelectorAll('.modal__toggle')
     let nav = document.querySelector('.header__nav')
+    let mainModal = document.querySelector('.main__modal')
+    let mainBg = document.querySelector('.main__modalBg')
+    let mainCompleted = document.querySelector('.main__completed')
     buttons.forEach(button =>{
         button.addEventListener('click', ()=>{
-            console.log(button)
-            nav.classList.toggle('hide')
-            buttons[0].classList.toggle('hide')
-            body.classList.toggle('overflow--hidden')
+            if(button.classList.contains('header__toggle')){
+                nav.classList.toggle('hide')
+                buttons[0].classList.toggle('hide')
+                body.classList.toggle('overflow--hidden')
+            } else if(button.classList.contains('pledge__toggle')){
+                mainModal.classList.toggle('hide')
+                mainBg.classList.toggle('hide')
+            } else if(button.classList.contains('toggle__completed--show')){
+                mainModal.classList.toggle('hide')
+                mainCompleted.classList.toggle('hide')
+            } else if(button.classList.contains('toggle__completed--hide')){
+                mainCompleted.classList.toggle('hide')
+                mainBg.classList.toggle('hide')
+            }
+            console.log(button.classList)
         })
     })
 }
